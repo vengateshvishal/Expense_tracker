fd7d5173dd811652170340efafd9b0075a839cb9
Vishal <vishal9345566@gmail.com>
2026-01-26 12:55:33 +0530

    google font added

--- lib/main.dart
+++ lib/main.dart
@@ -1,18 +1,29 @@
 import 'package:expense_tracker/Services/authLayout.dart';
+import 'package:expense_tracker/firebase_options.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
+  TextTheme? get textTheme => null;
+
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(debugShowCheckedModeBanner: false, home: AuthLayout());
+    return MaterialApp(
+      theme: ThemeData(
+        textTheme: GoogleFonts.nunitoTextTheme(textTheme)),
+      debugShowCheckedModeBanner: false,
+      home: AuthLayout(),
+    );
   }
 }
